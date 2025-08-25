290ad9fe572cae220b150b367fd30ebdc3165108
haoyuyin <haoyuyin@cecbrain.com>
2025-08-25 15:28:13 +0800

    update

--- projects/ch02/03designerDialog/gotocelldialog.cpp
+++ projects/ch02/03designerDialog/gotocelldialog.cpp
@@ -1,23 +1,24 @@
 #include "gotocelldialog.h"
+#include <QRegExpValidator>
 #include "ui_gotocelldialog.h"
 
 GoToCellDialog::GoToCellDialog(QWidget *parent)
     : QDialog(parent)
 {
-    ui->setupUi(this);
+    setupUi(this);
     QRegExpValidator *validator = new QRegExpValidator(QRegExp("[A-Za-z][1-9][0-9]{0,2}"), this);
-    ui->lineEdit->setValidator(validator);
+    lineEdit->setValidator(validator);
     // 连接文本变化信号到槽
-    connect(ui->lineEdit, &QLineEdit::textChanged, this, &GoToCellDialog::on_lineEdit_textChanged);
+    connect(this->okButton, &QPushButton::clicked, this, &GoToCellDialog::accept);
+    connect(this->cancelButton, &QPushButton::clicked, this, &GoToCellDialog::reject);
+    connect(this->buttonBox, &QDialogButtonBox::rejected, this, &GoToCellDialog::reject);
+    connect(this->buttonBox, &QDialogButtonBox::accepted, this, &GoToCellDialog::accept);
 }
 
-GoToCellDialog::~GoToCellDialog()
-{
-    delete ui;
-}
+GoToCellDialog::~GoToCellDialog() {}
 
 void GoToCellDialog::on_lineEdit_textChanged(const QString &text)
 {
     // 只有输入框有内容时才启用OK按钮
-    ui->okButton->setEnabled(!text.isEmpty());
+    this->okButton->setEnabled(!text.isEmpty());
 }
